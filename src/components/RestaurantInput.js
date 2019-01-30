b03d824317475cca60263fc31210cdc4bcdb9243
Peter Kreppein <pmkreppein@gmail.com>
2019-01-30 06:27:03 +0000

    Automatically backed up by Learn

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { addRestaurant } from '../actions/restaurants';
 import { connect } from 'react-redux';
-
+import { bindActionCreators } from 'redux'; 
 export class RestaurantInput extends Component {
 
   state = {
